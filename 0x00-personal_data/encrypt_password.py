ed253268ba4afcb0584535033b1bad7e1f654024
riyo3350g <yechouben@gamail.com>
2024-02-08 12:17:25 +0100

    Add Task 6 Req

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     , which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ function that expects a hashed_password bytes
+    and a password string arguments and returns a boolean.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
