e368f1ee5f1cc0073e264caf3de9c95ee97e6af5
riyo3350g <yechouben@gamail.com>
2024-02-08 12:12:49 +0100

    Add Task 5 Req

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Encrypting passwords Module"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ function that expects one string argument name password and returns a salted, hashed password, which is a byte string."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
