497a1402f44f2b9111543d4ffb4f8ab244d19abd
riyo3350g <yechouben@gamail.com>
2024-02-13 18:56:02 +0100

    Add Task 8 Req

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic auth module"""
 from api.v1.auth.auth import Auth
 from typing import TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,17 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str,
+                                           ) -> str:
+        """method that returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header)\
+                .decode('utf-8')
+        except Exception:
+            return None
