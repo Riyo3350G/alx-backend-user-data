5bb408a90a9c4b33524b7db4697e357a007995cf
riyo3350g <yechouben@gamail.com>
2024-02-13 20:46:41 +0100

    Add Task 13 ADV Req Fix 1

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,21 +10,18 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns True if the path is not in
         the list of strings excluded_paths"""
-        if path is None:
+        if not path:
             return True
-        if path[len(path) - 1] != '/':
-            path += '/'
-        astericks = [stars[:-1]
-                     for stars in excluded_paths if stars[-1] == '*']
-        for star in astericks:
-            if path.startswith(star):
+        if not excluded_paths:
+            return True
+        path = path.rstrip("/")
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*") and \
+                    path.startswith(excluded_path[:-1]):
                 return False
-        if excluded_paths is None or not excluded_paths:
-            return True
-        if path not in excluded_paths:
-            return True
-        if path in excluded_paths:
-            return False
+            elif path == excluded_path.rstrip("/"):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """method that returns None - request"""
