e3e32e6686ee3e576503e510d8372601a2ae4156
riyo3350g <yechouben@gamail.com>
2024-02-13 19:19:25 +0100

    Add Task 10 Req

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Basic auth module"""
+from models.user import User
 from api.v1.auth.auth import Auth
 from typing import TypeVar, Tuple
 import base64
@@ -46,3 +47,29 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """method that returns the User
+        instance based on his email and password"""
+        # Return None if user_email or user_pwd is not provided or not a string
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        # Use the search class method of User
+        # to lookup the list of users based on their email
+        users = User.search({'email': user_email})
+
+        # Return None if no user found in the database
+        if not users:
+            return None
+
+        # Check if user_pwd is the password of the User instance found
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        # Return None if user_pwd is not the password of any User instance
+        return None
