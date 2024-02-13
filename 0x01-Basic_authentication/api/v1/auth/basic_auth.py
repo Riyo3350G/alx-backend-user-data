e2df29c797c04f9fcd5c697a24d2bfa4d20e0eb9
riyo3350g <yechouben@gamail.com>
2024-02-13 18:47:03 +0100

    Add Task 7 Req

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,15 @@ from typing import TypeVar
 
 class BasicAuth(Auth):
     """BasicAuth class"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """method returns the Base64 part of the
+        Authorization header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
