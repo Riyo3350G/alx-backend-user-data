3efdab428911639c9af697d80f69287f6cfe7d13
riyo3350g <yechouben@gamail.com>
2024-02-14 20:35:29 +0100

    Add Task 4 Req

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Auth module"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -32,3 +33,9 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """"method that returns None - request"""
         return None
+
+    def session_cookie(self, request=None):
+        """method that returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv('SESSION_NAME'), None)
