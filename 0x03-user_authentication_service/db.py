a0da91953babfcbc04c52e85b6abec83d5890b1a
riyo3350g <yechouben@gamail.com>
2024-02-20 19:00:44 +0100

    Add Task 3 Req

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,3 +50,14 @@ class DB:
             raise NoResultFound
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """method that updates the user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
